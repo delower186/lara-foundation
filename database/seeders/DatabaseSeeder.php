61e20b83ce265b583e8820d3010952365904b479
Md. Delower Hossain <info@delower.me>
2022-07-10 22:02:52 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)->create();
+
+        $roles = ['admin','editor','writer'];
+
+        foreach($roles as $role){
+            Role::create(['name' => $role ]);
+        }
+
+        foreach(User::all() as $user){
+            foreach(Role::all() as $role){
+                $user->roles()->attach($role->id);
+            }
+        }
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
